5decf44da67afbda27dba74995351dc46b13c18b
chengchenghappy <1306073684@qq.com>
2018-09-15 19:43:47 +0800

    music

---
+++ 音视频/js/index.js
@@ -0,0 +1,9 @@
+var a=document.getElementsByTagName("a");
+var play=document.getElementById("play");
+for(var i=0;i<a.length;i++){
+	a[i].onclick=function music(){
+		var prev=this.previousElementSibling;
+			play.src=prev.src;
+			play.play();
+	}
+}
