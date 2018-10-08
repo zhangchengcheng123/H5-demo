218ec2181ce8268aaff9129072a7f12c68825eae
chengchenghappy <1306073684@qq.com>
2018-10-08 10:06:04 +0800

    music

--- 音视频/js/index.js
+++ 音视频/js/index.js
@@ -3,7 +3,7 @@ var play=document.getElementById("play");
 for(var i=0;i<a.length;i++){
 	a[i].onclick=function music(){
 		var prev=this.previousElementSibling;
-			play.src=prev.src;
-			play.play();
+		play.src=prev.src;
+		play.play();
 	}
 }
